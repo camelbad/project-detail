e428549438112edc7d817b1928d37484d744ffab
camelbad <38832784+camelbad@users.noreply.github.com>
2023-01-24 12:26:22 -0800

    Add files via upload

---
+++ algorithms/BST-AVL-tree/BST-AVL-tree/bst/assignment1/bst.c
@@ -0,0 +1,166 @@
+﻿#include "bst.h"
+
+BST*  creatNewBst( )//Initialize the bst tree
+{
+	BST *recorder= (BST*)malloc(sizeof (BST));//Allocation space
+	
+	recorder->root= NULL;//Root node is set to empty
+	
+	return recorder;
+}
+
+
+
+BSTNodePtr min_node(BSTNodePtr self) //Finding the smallest node
+{
+	BSTNodePtr current = self;
+	while (current->left != NULL)//When the last node is not reached
+	{
+		current = current->left;//left
+	}
+	return current;
+}
+
+
+BSTNodePtr delete_bst_node(BSTNodePtr root, long id)//Delete node
+{
+	if (root != NULL) {
+		if (id == root->data) { // found item 
+			if (root->left != NULL && root->right != NULL) {
+				// two child case 
+				BSTNodePtr successor = min_node(root->right);
+				root->data = successor->data;
+				root->right = delete_bst_node(root->right, root->data);
+			}
+			else { // one or zero child case 
+				BSTNodePtr to_free = root;
+				if (root->left) {
+					root = root->left;
+				}
+				else {
+					root = root->right;
+				}
+				destroy_list(to_free->courses.head);
+				free(to_free);
+			}
+		}
+		else if (id < root->data) {
+			root->left = delete_bst_node(root->left, id);
+		}
+		else {
+			root->right = delete_bst_node(root->right, id);
+		}
+	}
+	return root;
+}
+
+BSTNodePtr find_bst_node(BSTNodePtr self, long n)//Find node
+{
+	if (self == NULL || (n == self->data)) {//Restrictions
+		return self;
+	}
+	else if (n < self->data) {//Recursively traverses the entire tree
+		return find_bst_node(self->left, n);
+	}
+	else {
+		return find_bst_node(self->right, n);
+	}
+}
+
+BSTNodePtr searchById(BST *recorder, long n) {
+	return find_bst_node(recorder->root, n);
+}
+
+BSTNodePtr insert_bst_node(BSTNodePtr node, long id)//Insert node
+{
+	if (node == NULL) { /* found where to put it*/
+		node = malloc(sizeof *node);
+		node->data = id;
+		node->courses= new_courselist();
+		node->left = node->right = NULL;
+	}
+	else if (id < node->data) {//Recursively traverses the entire tree
+		node->left = insert_bst_node(node->left, id);
+	}
+	else if (id >node->data) {
+		node->right = insert_bst_node(node->right, id);
+	}
+	return node;
+}
+
+
+
+
+
+
+	allCourseStastic* displayInfo(BSTNodePtr recorderRoot, allCourseStastic *courselist)//Get course statistics
+	{
+
+		if (recorderRoot == NULL)
+		{
+			return courselist;
+		}
+		displayInfo(recorderRoot->left, courselist);//in order recursive traversal
+		ListNodePtr coursePtr = recorderRoot->courses.head;//For each node, get the head pointer for this node's course list
+		while (coursePtr != NULL)
+		{
+
+			char* courseName = coursePtr->data;//Get the first course name
+			courseNodePtr coursePtrTemp = findCourse(courseName, courselist);//Determine whether this course already exists in the course statistics list
+
+			if (coursePtrTemp == NULL)
+			{
+				insertCourseSta(courseName, courselist);//If this course is not in the course statistics list, insert this course into the course statistics list
+
+			}
+			else
+			{
+				addCourseCounter(coursePtrTemp);//If it already exists, the course counter adds one
+
+			}
+			coursePtr = coursePtr->next;//move pointer to next
+		}
+
+		displayInfo(recorderRoot->right, courselist);
+		return courselist;
+	}
+
+
+	void destory(BSTNodePtr recorderRoot)//delete tree
+	{
+		if (recorderRoot == NULL)
+			return;
+		if (recorderRoot->left!= NULL)
+			destory(recorderRoot->left);
+		if (recorderRoot->right!= NULL)
+			destory(recorderRoot->right);
+		if (recorderRoot->courses.head!=NULL)
+		{
+			destroy_list(&recorderRoot->courses);
+		}
+		free(recorderRoot);
+	}
+
+
+
+
+
+
+	/*void printRecorder(BSTNodePtr node) {
+	if (node == NULL)
+	{
+	return;
+	}
+	printRecorder(node->left);
+	printf("%ld\n", node->data);
+	ListNodePtr temp = node->courses.head;
+	while (temp != NULL)
+	{
+	printf("%s\n", temp->data);
+	temp = temp->next;
+	}
+
+
+
+	printRecorder(node->right);
+	}*/
---
+++ algorithms/BST-AVL-tree/BST-AVL-tree/bst/assignment1/stringlist.c
@@ -0,0 +1,133 @@
+﻿#include "STRINGLIST.H"
+#include <string.h>
+#include "bst.h"
+
+List new_courselist()/*Initialize the course list*/
+{
+		List temp;
+		temp.head = NULL;//set Head pointer to null
+		return temp;
+}
+
+
+
+void enroll_a_Course_Tostu(char *courseName, List *courseList)//Registration Course, Insertion of List
+{
+	ListNodePtr new_course = malloc(sizeof *new_course);//Get new nodes and allocate space
+	new_course->data =courseName ;//Assign the new the new course name to the node's data field 
+	//Head insertion
+	new_course->next = courseList->head;//Put new node before the original head node
+	courseList->head = new_course;//Point the head node to the new node	
+}
+
+void deleteCourse(char *courseName, List* course)//unenroll course
+{
+	ListNodePtr current = course->head;//Current pointer
+	ListNodePtr prev = NULL;//Previous position pointer
+
+	while (current != NULL)
+	{
+		if (strcmp (current->data, courseName) ==0)//When the current node's course content matches the  course
+		{
+			if (prev == NULL) {          // front of list
+				course->head = current->next;
+				free(current);//free node
+				current = course->head;
+			}
+			else {                    // middle of list
+				prev->next = current->next;
+				free(current);
+				current = prev->next;
+			}
+		}
+		else {//If the current node does not meet the requirements, move the pointer to next
+			prev = current;
+			current = current->next;
+		}
+	}
+
+}
+
+showCoursesOfStu(List *courseList)//display all courses for certain student
+{
+	ListNodePtr current = courseList->head;
+	while (current->next!=NULL)
+	{
+		printf("%s \n", current->data);
+		current = current->next;
+	}
+}
+
+courseNodePtr findCourse(char* course, allCourseStastic *courselist)//Determine wether the course exists and return the course pointer if it exists
+
+{
+	courseNodePtr temp = courselist->head;
+	while (temp != NULL && strcmp(temp->courseName, course) != 0)//If not
+	{
+
+		temp = temp->next;//move pointer to next
+	}
+
+	return temp;
+
+}
+
+
+
+allCourseStastic creatStasticList()//Initialize Course Statistics Linked List
+{
+	allCourseStastic temp;
+	temp.head = NULL;
+	return temp;
+}
+
+insertCourseSta(char *courseName, allCourseStastic *courseList)//Course Statistics Linked List Insertion
+{
+	courseNodePtr current = courseList->head;
+	courseNodePtr prev = NULL;
+	//creat new node
+	courseNodePtr new_node = malloc(sizeof *new_node);
+	new_node->courseName = courseName;
+	new_node->NumOfStu = 1;//The initial value is 1
+	new_node->next = NULL;
+	//
+
+	while (current != NULL && strcmp(current->courseName, courseName)<0)
+	{
+		prev = current;
+		current = current->next;
+	}
+
+	if (current == courseList->head)
+	{ // at front
+		new_node->next = current;
+		courseList->head = new_node;
+	}
+	else
+	{                     // middle
+		new_node->next = current;
+		prev->next = new_node;
+	}
+
+}
+
+addCourseCounter(courseNodePtr Course)//Course counter plus one
+{
+	Course->NumOfStu = Course->NumOfStu + 1;
+}
+
+
+
+void destroy_list(List *self)//Destroy the linked list
+{
+	ListNodePtr current = self->head;
+	while (current != NULL)
+	{
+		ListNodePtr to_free = current;//Assign the value of the current pointer to the node will be release
+		current = current->next;//move pointer to next
+		free(to_free);//free space
+	}
+	self->head = NULL;
+}
+
+

cc565c1f424ad0229eee1e383e7cad144146e94e
camelbad <38832784+camelbad@users.noreply.github.com>
2023-01-24 12:10:49 -0800

    Add files via upload

---
+++ BST-AVL-tree/avl/assignment1/AVL.c
@@ -0,0 +1,337 @@
+﻿#include "AVL.h"
+
+int max(int a ,int b)
+{
+	int c;
+	if (a > b)
+		c = a;
+	else
+		c = b;
+	return c; 
+}
+
+int hight(AVLNodePtr avltreeRoot)
+{
+	return avltreeRoot->height;
+}
+
+
+AVL*  creatNewAVL( )//Initialize the bst tree
+{
+	AVL *recorder= (AVL*)malloc(sizeof (AVL));//Allocation space
+
+	
+	recorder->root= NULL;//Root node is set to empty
+	
+	return recorder;
+}
+
+int avltree_height(AVL tree)
+ {
+	int height = 0;
+	if (tree.root == NULL)
+	{
+		height = -1;
+	}
+	else
+	{
+		height = tree.root->height;
+	}
+	return height;
+ }
+
+AVLNodePtr min_node(AVLNodePtr self) //Finding the smallest node
+{
+	AVLNodePtr current = self;
+	while (current->left != NULL)//When the last node is not reached
+	{
+		current = current->left;//left
+	}
+	return current;
+}
+
+
+//LL：
+
+
+AVLNodePtr left_left_rotation(AVLNodePtr k2root)
+ {
+    AVL k1;
+
+    k1.root = k2root->left;
+    k2root->left = k1.root->right;
+    k1.root->right = k2root;
+
+    k2root->height = max(hight(k2root->left), hight(k2root->right)) + 1;
+    k1.root->height = max(hight(k1.root->left), k2root->height) + 1;
+
+     return k1.root;
+ }
+
+//RR
+ AVLNodePtr right_right_rotation(AVLNodePtr k1root)
+     {
+	 AVL k2;
+
+     k2.root = k1root->right;
+     k1root->right = k2.root->left;
+     k2.root->left = k1root;
+	 k1root->height = max(hight(k1root->left), hight(k1root->right)) + 1;
+	 k2.root->height = max(hight(k2.root->right), k1root->height) + 1;
+
+	     return k2.root;
+	 }
+
+ //LR
+
+	 AVLNodePtr left_right_rotation(AVLNodePtr k3root)
+	  {
+	    k3root->left = right_right_rotation(k3root->left);
+	
+        return left_left_rotation(k3root);
+	  }
+ 
+// * RL
+	 AVLNodePtr right_left_rotation(AVLNodePtr k1root)
+	 {
+		 k1root->right = left_left_rotation(k1root->right);
+
+		 return right_right_rotation(k1root);
+
+	 }
+
+
+
+	 //  
+	 AVLNodePtr avltree_create_node(int key, AVLNodePtr *left, AVLNodePtr* right)
+	 {
+		 AVLNodePtr p;
+
+		 if ((p = (AVLNodePtr *)malloc(sizeof(AVLNodePtr))) == NULL)
+			 return NULL;
+		 p->data = key;
+		 p->courses = new_courselist();
+		 p->height = 0;
+		 p->left = left;
+		 p->right = right;
+		 return p;
+	 }
+
+	 AVLNodePtr avltree_maximum(AVLNodePtr treeroot)
+		{
+		    if (treeroot == NULL)
+			        return NULL;
+		
+			    while (treeroot->right != NULL)
+			       treeroot = treeroot->right;
+	    return treeroot;
+		}
+
+
+
+
+AVLNodePtr delete_avl_node(AVLNodePtr treeroot, AVLNodePtr targetNode)//Delete node
+{
+	
+	 if (treeroot == NULL || targetNode == NULL)
+	 return NULL;
+     if (targetNode->data < treeroot->data)        // The node need to be deleted is in "tree's left subtree"
+	    {
+	         treeroot->left = delete_avl_node(treeroot->left, targetNode);
+	        // After the node is deleted, if the AVL tree loses its balance, adjust it accordingly.
+	         if (hight(treeroot->right) - hight(treeroot->left) == 2)
+	         {
+		            AVLNodePtr r = treeroot->right;
+		             if (hight(r->left) > hight(r->right))
+		                 treeroot = right_left_rotation(treeroot);
+		            else
+			                 treeroot = right_right_rotation(treeroot);
+			 }
+	    }
+	  else if (targetNode->data > treeroot->data)// The node to be deleted is in the "tree's right subtree"
+		     {
+		        treeroot->right = delete_avl_node(treeroot->right, targetNode);
+		        // After the node is deleted, if the AVL tree loses its balance, adjust it accordingly.
+		        if (hight(treeroot->left) - hight(treeroot->right) == 2)
+			        {
+			             AVLNodePtr l = treeroot->left;
+		             if (hight(l->right) > hight(l->left))
+			                 treeroot = left_right_rotation(treeroot);
+			           else
+			                treeroot = left_left_rotation(treeroot);
+		            }
+		    }
+     else    // 
+		     {
+		        //The children of the tree are not empty
+		        if ((treeroot->left) && (treeroot->right))
+		        {
+		            if (hight(treeroot->left) > hight(treeroot->right))
+		            {
+		                // If the tree's left subtree is higher than the right subtree;
+				    AVLNodePtr max = avltree_maximum(treeroot->left);
+	                treeroot->data = max->data;
+		            treeroot->left = delete_avl_node(treeroot->left, max);
+		             }
+			      else
+			         {
+		                // If the tree's left subtree is not taller than the right subtree (that is, they are equal, or the right subtree is 1 higher than the left subtree)
+			           AVLNodePtr min = avltree_maximum(treeroot->right);
+	                   treeroot->data = min->data;
+		               treeroot->right = delete_avl_node(treeroot->right, min);
+		             }
+		       }
+		    else
+		      {
+		            AVLNodePtr tmp = treeroot;
+		            treeroot = treeroot->left ? treeroot->left : treeroot->right;
+		            free(tmp);
+		        }
+		     }
+		
+		   return treeroot;
+		}
+
+
+AVLNodePtr find_avl_node(AVLNodePtr self, long n)//Find node
+{
+	if (self == NULL || (n == self->data)) {//Restrictions
+		return self;
+	}
+	else if (n < self->data) {//Recursively traverses the entire tree
+		return find_avl_node(self->left, n);
+	}
+	else {
+		return find_avl_node(self->right, n);
+	}
+}
+
+AVLNodePtr searchById(AVL *recorder, long n) {
+	return find_avl_node(recorder->root, n);
+}
+
+AVLNodePtr insert_avl_node(AVLNodePtr rootnode, long id)//Insert node
+{
+
+	
+		     if (rootnode == NULL)
+			     {
+			       
+				         rootnode = avltree_create_node(id, NULL, NULL);
+			         if (rootnode == NULL)
+				        {
+				             printf("ERROR: create avltree node failed!\n");
+				             return NULL;
+				         }
+			     }
+		     else if (id < rootnode->data) // The key should be inserted into the "tree's left subtree"
+			     {
+			         rootnode->left = insert_avl_node(rootnode->left, id);
+			         // After inserting the node, if the AVL tree loses its balance, adjust accordingly.
+				         if (hight(rootnode->left) - hight(rootnode->right) == 2)
+				         {
+				             if (id < rootnode->left->data)
+					                 rootnode = left_left_rotation(rootnode);
+				             else
+					                 rootnode = left_right_rotation(rootnode);
+				         }
+			     }
+		     else if (id > rootnode->data) // The key should be inserted into the "tree's right subtree"
+			     {
+			        rootnode->right = insert_avl_node(rootnode->right, id);
+			       // After inserting the node, if the AVL tree loses its balance, adjust accordingly.
+				         if (hight(rootnode->right) - hight(rootnode->left) == 2)
+				         {
+				             if (id > rootnode->right->data)
+					                 rootnode = right_right_rotation(rootnode);
+				             else
+					                 rootnode = right_left_rotation(rootnode);
+				         }
+			     }
+		     else //key == tree->key)
+			     {
+			         printf("fail \n");
+			     }
+		
+			     rootnode->height = max(hight(rootnode->left), hight(rootnode->right)) + 1;
+		
+			     return rootnode;
+		
+}
+
+
+
+
+
+
+	allCourseStastic* displayInfo(AVLNodePtr recorderRoot, allCourseStastic *courselist)//Get course statistics
+	{
+
+		if (recorderRoot == NULL)
+		{
+			return courselist;
+		}
+		displayInfo(recorderRoot->left, courselist);//in order recursive traversal
+		ListNodePtr coursePtr = recorderRoot->courses.head;//For each node, get the head pointer for this node's course list
+		while (coursePtr != NULL)
+		{
+
+			char* courseName = coursePtr->data;//Get the first course name
+			courseNodePtr coursePtrTemp = findCourse(courseName, courselist);//Determine whether this course already exists in the course statistics list
+
+			if (coursePtrTemp == NULL)
+			{
+				insertCourseSta(courseName, courselist);//If this course is not in the course statistics list, insert this course into the course statistics list
+
+			}
+			else
+			{
+				addCourseCounter(coursePtrTemp);//If it already exists, the course counter adds one
+
+			}
+			coursePtr = coursePtr->next;//move pointer to next
+		}
+
+		displayInfo(recorderRoot->right, courselist);
+		return courselist;
+	}
+
+
+	void destory(AVLNodePtr recorderRoot)//delete tree
+	{
+		if (recorderRoot == NULL)
+			return;
+		if (recorderRoot->left!= NULL)
+			destory(recorderRoot->left);
+		if (recorderRoot->right!= NULL)
+			destory(recorderRoot->right);
+		if (recorderRoot->courses.head!=NULL)
+		{
+			destroy_list(&recorderRoot->courses);
+		}
+		free(recorderRoot);
+	}
+
+
+
+
+
+
+	/*void printRecorder(BSTNodePtr node) {
+	if (node == NULL)
+	{
+	return;
+	}
+	printRecorder(node->left);
+	printf("%ld\n", node->data);
+	ListNodePtr temp = node->courses.head;
+	while (temp != NULL)
+	{
+	printf("%s\n", temp->data);
+	temp = temp->next;
+	}
+
+
+
+	printRecorder(node->right);
+	}*/
---
+++ BST-AVL-tree/avl/assignment1/assign1AVL.c
@@ -0,0 +1,188 @@
+﻿#include "ASSIGN1AVL.H"
+#include <stdio.h>
+#include <stdlib.h> 
+
+
+
+void addStudent(long id, AVL * recorder)//add student
+{
+	recorder->root = insert_avl_node(recorder->root, id);
+}
+
+
+void deleteStu(long id, AVL *recorder)//delete student
+{
+	AVLNodePtr targetNode = find_avl_node(recorder->root, id);
+	recorder->root = delete_avl_node(recorder->root, targetNode);
+
+}
+
+void addCourseToStu(char *courseName, long id, AVL *recorder)//enroll course
+{
+	
+	AVLNodePtr a=searchById(recorder,id);//locate student by id
+
+	enroll_a_Course_Tostu(courseName, &a->courses);//enroll a course to a student
+}
+
+
+
+void delCourse(char *courseName, long id, AVL *recorder)//delete course
+{
+	AVLNodePtr a = searchById(recorder, id);//locate student
+	deleteCourse(courseName, &a->courses);//delete course
+
+}
+
+
+
+outputCourseSta(allCourseStastic *result)//print the student number of certain courses
+{
+	courseNodePtr temp;
+	temp = result->head;//get the head pointer of the static result
+	while (temp != NULL)
+	{
+		printf("%s ", temp->courseName);//display course name
+		printf("%d\n", temp->NumOfStu);//display student number
+		temp = temp->next;//move pointer to next
+	}
+}
+
+void displayStuListOfCourse(char *courseName, AVLNodePtr recorderRoot)//ger the student number of certain course
+{
+
+	if (recorderRoot == NULL)//quit if Null
+	{
+		return ;
+	}
+	displayStuListOfCourse(courseName, recorderRoot->left);//Recursively in order, traverse each node
+	ListNodePtr coursePtr = recorderRoot->courses.head;//For each node, get the head of the course list
+	while (coursePtr != NULL)
+	{
+		if (strcmp(coursePtr->data, courseName) == 0)//If this student registers this course
+		{
+			printf("%ld\n", recorderRoot->data);//Print student names
+		}
+		
+		coursePtr = coursePtr->next;//move pointer to next
+	}
+
+	displayStuListOfCourse(courseName, recorderRoot->right);
+
+}
+
+void displayCoursesOfStu(long id, AVL *recorder)//Shows all courses registered for a particular student
+{
+	AVLNodePtr result= find_avl_node(recorder->root, id);//Target students, based on ID
+	ListNodePtr course = result->courses.head;//Get the head address of the course link list
+	while (course != NULL)
+	{
+		printf("%s\n",course->data);//Print Course
+		course = course->next;//move pointer to next
+	}
+}
+
+
+
+
+
+
+
+
+/**********************************************************************************************************************************************************************************************************************************************************************/
+
+void main()
+{
+	int userselect=0;//User selection f
+	long stuId;//student ID
+	AVL *recorder= creatNewAVL();//initializing BST tree for recorder
+	char *courseName;//course name
+	printf("Student Registration Information System\n");
+	printf("\n\n\n");
+	printf("0. Quit\n");
+	printf("1. Add a student\n");
+	printf("2. Remove a student\n");
+	printf("3. Enrol student in a course\n");
+	printf("4. Un-enrol student from a course\n");
+	printf("5. Print a summary of courses and the number of students enrolled in each course\n");
+	printf("6. Print an ordered list of students enrolled in a course\n");
+	printf("7. Print a list of courses that a given student is enrolled in\n");
+	
+	
+	
+	//get user input
+	
+	
+	
+	
+	do
+	
+	{
+		printf("Pleast input a number from 1~8\n");
+		scanf("%d", &userselect);//Get which function the user chooses
+		switch (userselect)
+		{
+		case 1:
+			printf("Please input the student ID which you want to add\n");
+			scanf("%ld", &stuId);//Get the student ID  want to add
+			
+				addStudent(stuId, recorder);
+		
+			break;
+		case 2:
+			printf("Please input the student ID which you want to delete\n");
+			scanf("%ld", &stuId);//Get the student ID that needs to be deleted
+			deleteStu(stuId, recorder);
+			break;
+		case 3:
+			printf("Please input the student ID and the course you want to enrol for that studet\n");
+			printf("Please input the student ID \n");
+			scanf("%ld", &stuId);//Get the student ID 
+			printf("Please input the course name\n");
+			char* courseName = (char*)malloc(sizeof(char) * 100);
+			scanf("%s", courseName);//get course name
+			addCourseToStu(courseName, stuId, recorder);
+			break;
+		case 4:
+			printf("Please input the student ID and the course you want to delete for that studet\n");
+			printf("Please input the student ID \n");
+			scanf("%ld", &stuId);//Get the student ID  want to add
+			printf("Please input the course name\n");
+			char* courseName2 = (char*)malloc(sizeof(char) * 100);
+			scanf("%s", courseName2);//get course name
+			delCourse(courseName2, stuId, recorder);
+			break;
+		case 5:
+			printf("Print a summary of courses and the number of students enrolled in each course\n");
+			allCourseStastic courselist= creatStasticList();//Initialize a new linked list to store course statistics
+			outputCourseSta(displayInfo(recorder->root,&courselist));//Get course statistics result and display
+			
+			break;
+		case 6:
+			printf(" Print an ordered list of students enrolled in a course\n");
+			printf(" please in put a course name\n");
+			scanf("%s", courseName);//Get the course name
+			displayStuListOfCourse(courseName, recorder->root);
+			break;
+		case 7:
+			printf(" Print a list of courses that a given student is enrolled in\n");
+			printf(" Please input a student ID\n");
+			scanf("%ld", &stuId);//Get student ID
+			printf("Enrolment for %ld\n",stuId);
+			displayCoursesOfStu(stuId, recorder);
+			break;
+		case 0:
+			printf(" Quit\n");
+			destory(recorder->root);//Free space
+			exit(0);
+			break;
+
+
+		default:
+			printf(" Wrong input, please input int Number 1~8\n");
+			break;
+		}
+
+	} while (userselect != 0);//When the user selects 0 then exit.
+		
+}
